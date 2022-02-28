1e5494a78c9b7d17a09e801fc9ef84b36c804155
zhouqichun <2925009749@qq.com>
2022-02-28 15:01:21 +0800

    improve

---
+++ vue-pure/assets/redirect.15293526.js
@@ -0,0 +1,1 @@
+import{d as e,af as a,u as r,f as s,e as t}from"./vue.5e6d7e94.js";import"./element-plus.8b2bac93.js";const u=e({setup(e){const{currentRoute:u,replace:p}=a(),{params:o,query:n}=r(u),{path:c}=o;return p({path:"/"+(Array.isArray(c)?c.join("/"):c),query:n}),(e,a)=>(t(),s("div"))}});export{u as default};
